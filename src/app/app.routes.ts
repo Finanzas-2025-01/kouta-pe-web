3f4eae0a616f399fd38ef7bb6a4e06a09710e550
Neorise456 <defneorise@gmail.com>
2025-07-03 18:50:09 -0500

    feat: add HTTP client with interceptor and implement auth guard for main route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 import {MainComponent} from './public/pages/main/main.component';
 import {SignInComponent} from './iam/pages/sign-in/sign-in.component';
 import {SignUpComponent} from './iam/pages/sign-up/sign-up.component';
+import {authGuard} from './iam/services/auth-guard';
 
 
 export const routes: Routes = [
-  {path: '/' , component: MainComponent},
+  {path: '' , component: MainComponent, canActivate:[authGuard]},
   {path: 'sign-in',component: SignInComponent},
   {path: 'sign-up', component: SignUpComponent},
-  {path : '', redirectTo: '/', pathMatch: 'full'},
 ];
