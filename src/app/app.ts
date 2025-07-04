afae3d64fe474d644c8b33164466bc3cd0d1d718
Al3xiel <gabriel280104casas@gmail.com>
2025-07-04 17:16:30 -0500

    refactor: remove header and footer components from app structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {BondFooterComponent} from './public/components/bond-footer/bond-footer.component';
-import {BondHeaderComponent} from './public/components/bond-header/bond-header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BondFooterComponent, BondHeaderComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
