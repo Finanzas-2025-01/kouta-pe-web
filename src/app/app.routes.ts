0f41adbf1e670ac1f8b18d9ad2e2eafc6a8899e8
Al3xiel <gabriel280104casas@gmail.com>
2025-07-04 22:37:35 -0500

    refactor(bonds): reorganize bonds component structure and update paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import {SignInComponent} from './iam/pages/sign-in/sign-in.component';
 import {SignUpComponent} from './iam/pages/sign-up/sign-up.component';
 import {authGuard} from './iam/services/auth-guard';
 import {IssuersBonds} from './public/pages/issuers-bonds/issuers-bonds';
-import {Bonds} from './public/pages/bonds/bonds';
+import {Bonds} from './bond-holders/pages/bonds/bonds';
 
 
 export const routes: Routes = [
