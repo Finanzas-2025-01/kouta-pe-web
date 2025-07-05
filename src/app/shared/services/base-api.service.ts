423400fd23d5bd0f12d1763920c3a7ff9b3d47e5
Al3xiel <gabriel280104casas@gmail.com>
2025-07-04 22:38:00 -0500

    refactor(api): rename BaseService to BaseApiService and update base URL structure

--- src/app/shared/services/base-api.service.ts
+++ src/app/shared/services/base-api.service.ts
@@ -4,15 +4,21 @@ import {inject} from '@angular/core';
 import {catchError, Observable, retry, throwError} from 'rxjs';
 import {environment} from "../../../environments/environment";
 
-export abstract class BaseService<T> {
+export abstract class BaseApiService<T> {
   /** HTTP headers configuration for JSON communication */
   protected httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
 
   /** Base URL for the server API */
-  protected serverBaseUrl: string = `http://localhost:8080/api/v1`;
+  protected baseUrl: string = `http://localhost:8080/api/v1`;
+
+  protected resourceEndPoint: string = '/resources';
 
   /** HTTP client for making API request */
   protected http: HttpClient = inject(HttpClient);
+
+  protected resourcePath(): string {
+    return `${this.baseUrl}${this.resourceEndPoint}`;
+  }
 
   protected handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
@@ -22,5 +28,6 @@ export abstract class BaseService<T> {
     }
     return throwError(() => new Error('Something bad happened; please try again later.'));
   }
+
 
 }
