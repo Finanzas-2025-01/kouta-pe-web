2ef02497bc14fb337c0f3a2a1253000d40ebac82
Al3xiel <gabriel280104casas@gmail.com>
2025-07-05 00:20:46 -0500

    feat(bonds): add BondCardList component to the bonds screen

--- src/app/bond-holders/pages/bonds/bonds.ts
+++ src/app/bond-holders/pages/bonds/bonds.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {MatCard} from '@angular/material/card';
+import {BondCardListComponent} from '../../components/bond-card-list/bond-card-list.component';
 
 @Component({
   selector: 'app-bonds',
-  imports: [],
+  imports: [
+    MatCard,
+    BondCardListComponent
+  ],
   templateUrl: './bonds.html',
   styleUrl: './bonds.css'
 })
